06a169aabadd4ec03b80d79e5a53a194cead4350
hajar fatheddine <fatheddinehajar@gmail.com>
2023-10-25 21:10:26 +0100

    creating the order microservice

---
+++ order-service/src/main/java/com/ecom/orderservice/dto/InventoryResponse.java
@@ -0,0 +1,14 @@
+package com.ecom.orderservice.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class InventoryResponse {
+    private String skuCode;
+    private boolean isInStock;
+}
