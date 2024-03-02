631a02cbb5894447d7b45ec06819c50e01d0d2ee
hajar fatheddine <fatheddinehajar@gmail.com>
2024-03-02 20:55:04 +0100

    circuit breaker

--- inventory-service/src/main/java/com/ecom/inventoryservice/services/InventoryService.java
+++ inventory-service/src/main/java/com/ecom/inventoryservice/services/InventoryService.java
@@ -3,6 +3,8 @@ package com.ecom.inventoryservice.services;
 import com.ecom.inventoryservice.dtos.inventory.InventoryResponse;
 import com.ecom.inventoryservice.repositories.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,11 +13,16 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode){
+        log.info("Wait Started");
+        Thread.sleep(20000);
+        log.info("Wait Ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory -> {
                     return InventoryResponse.builder().skuCode(inventory.getSkuCode())
--- order-service/src/main/java/com/ecom/orderservice/controllers/OrderController.java
+++ order-service/src/main/java/com/ecom/orderservice/controllers/OrderController.java
@@ -2,10 +2,15 @@ package com.ecom.orderservice.controllers;
 
 import com.ecom.orderservice.dtos.order.OrderRequest;
 import com.ecom.orderservice.services.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 
 @RestController
@@ -15,9 +20,14 @@ public class OrderController {
     private final OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody @Valid OrderRequest orderRequest){
-        orderService.placeOrder(orderRequest);
-        return "Order Placed Successfully";
+    @CircuitBreaker(name = "inventory",fallbackMethod = "fallBackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody @Valid OrderRequest orderRequest){
+        return  CompletableFuture.supplyAsync(()-> orderService.placeOrder(orderRequest));
     }
 
+    public CompletableFuture<String> fallBackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return CompletableFuture.supplyAsync(()->"Something went wrong, Please order later!");
+    }
 }
